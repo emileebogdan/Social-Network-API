c1f81c4397e2e18a5a0ac0ac862ad76450e12b36
emileebogdan <emileebogdan@gmail.com>
2022-11-18 18:22:48 -0700

    thought route

--- routes/api/thought-route.js
+++ routes/api/thought-route.js
@@ -1,2 +1,18 @@
 // similar to user route
-// 
+const router = require('express').Router();
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    // updateThought,
+
+} = require('../../controllers/thoughtController');
+
+// /api/thought
+router.route('/').get(getThought).post(createThought);
+
+// /api/thought/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+module.exports = router
